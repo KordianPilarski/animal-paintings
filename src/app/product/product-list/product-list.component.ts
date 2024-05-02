ce6f99bf5cfc3292ac670fe68c1351b30b166b60
Kordian <kordian.pilarski@capgemini.com>
2024-05-02 16:38:49 +0200

    make http request and set up enviroments

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+    console.log(this.products);
+    setTimeout(() => {
+      console.log(this.products);
+    }, 1000);
+  }
+
+  getProducts(): void {
+    console.log('called');
+    this.productService
+      .getProducts()
+      .subscribe((data) => (this.products = data));
+  }
 }
