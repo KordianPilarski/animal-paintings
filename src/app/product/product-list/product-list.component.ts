dfaa05cf5b9c36c8115194d3568b3e70e7f51187
Kordian <kordian.pilarski@capgemini.com>
2024-05-03 13:51:03 +0200

    add filter and sort for products

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -11,6 +11,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  filteredProducts: Product[] = [];
+  sortOrder: string = '';
 
   constructor(
     private productService: ProductService,
@@ -28,9 +30,10 @@ export class ProductListComponent implements OnInit {
 
   getProducts(): void {
     console.log('called');
-    this.productService
-      .getProducts()
-      .subscribe((data) => (this.products = data));
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+      this.filteredProducts = data;
+    });
   }
 
   addToCart(product: Product): void {
@@ -44,5 +47,25 @@ export class ProductListComponent implements OnInit {
         });
       },
     });
+  }
+
+  applyFilter(event: Event): void {
+    let searchTerm = (event.target as HTMLInputElement).value.toLowerCase();
+
+    this.filteredProducts = this.products.filter((product) =>
+      product.name.toLowerCase().includes(searchTerm)
+    );
+
+    this.sortProducts(this.sortOrder);
+  }
+
+  sortProducts(sortValue: string) {
+    this.sortOrder = sortValue;
+
+    if (this.sortOrder === 'priceLowHigh') {
+      this.filteredProducts.sort((a, b) => a.price - b.price);
+    } else if (this.sortOrder === 'priceHighLow') {
+      this.filteredProducts.sort((a, b) => b.price - a.price);
+    }
   }
 }
