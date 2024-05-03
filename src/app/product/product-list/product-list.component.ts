d15c5fdb69a68abce35a8cc47f3c460dfa3aa678
Kordian <kordian.pilarski@capgemini.com>
2024-05-03 09:45:48 +0200

    add checkout, calculate total, add snackbar

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/cart/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-list',
@@ -10,7 +12,11 @@ import { Product } from 'src/app/models/product';
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService,
+    private snackbar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.getProducts();
@@ -25,5 +31,18 @@ export class ProductListComponent implements OnInit {
     this.productService
       .getProducts()
       .subscribe((data) => (this.products = data));
+  }
+
+  addToCart(product: Product): void {
+    console.log(product);
+    this.cartService.addToCart(product).subscribe({
+      next: () => {
+        this.snackbar.open('Added to cart', '', {
+          duration: 1000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        });
+      },
+    });
   }
 }
